51bc752928a11eb173530e49f889995d25f9bf43
yukthi hettiarachchi <yukthihettiarachchissck@gmail.com>
2023-02-25 01:50:38 +0530

    button

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
+import Button from "@mui/material/Button";
 function App() {
-  return <div className="App">re</div>;
+  return (
+    <div className="App">
+      re
+      <Button variant="contained" color="blue">
+        gsgs
+      </Button>
+    </div>
+  );
 }
 
 export default App;
