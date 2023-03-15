2cd0ad2e72bdb53eb70dd505f11fb8201b9f33e1
Dilini Liyanage <121649435+dilini-liyanage@users.noreply.github.com>
2023-03-15 15:25:25 +0530

    Create Register page

--- client/src/pages/homePage/Home.jsx
+++ client/src/pages/homePage/Home.jsx
@@ -3,59 +3,62 @@ import homeBg from "../../assets/homeBG.jpg"
 import "./Home.css"
 import Button from "@mui/material/Button";
 import Header from "../../componets/Header";
-
+import Register from "../login/Register";
 
 
 const Home = () => {
-    return(
-        <div>
-        <Header/>
-        <div  className="Backgroung-img" >
-            <img src={homeBg} alt="" height="667px" width="100%" position="fixed"/>
-            <div className="content">
-                <p>
-                <div className="w1">LIVE YOUR</div>
-                <div className="w2">ADVENTURE</div>
-                <div className="p">Don't wait until tomorrow, discover your<br/>
-                adventure now and feel the sensation of<br/> closeness 
-                to nature around you</div>
-                </p>
-            </div>
-            <div className="box"/>
-            <div><input  className="destination1" placeholder="Where are you starting?"/></div>
-            <div><input className="destination2" placeholder="Where are you going?"/></div>
-            <Button href="/googleMapApi" sx={{marginLeft:"auto", color:"white", backgroundColor:"#E86E18", top:"-28.5vh", left:"87.5vh", borderRadius:"27px"}}>Go</Button>
-            </div>
-            <div>
-                <p className="p2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Odio neque veniam, earum, sed animi nesciunt architecto
-                    voluptate adipisci sunt, ut consectetur beatae eius inventore 
-                    quidem dolor error aperiam delectus. Odio.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Odio neque veniam, earum, sed animi nesciunt architecto
-                    voluptate adipisci sunt, ut consectetur beatae eius inventore 
-                    quidem dolor error aperiam delectus. Odio.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Odio neque veniam, earum, sed animi nesciunt architecto
-                    voluptate adipisci sunt, ut consectetur beatae eius inventore 
-                    quidem dolor error aperiam delectus. Odio.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Odio neque veniam, earum, sed animi nesciunt architecto
-                    voluptate adipisci sunt, ut consectetur beatae eius inventore 
-                    quidem dolor error aperiam delectus. Odio.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Odio neque veniam, earum, sed animi nesciunt architecto
-                    voluptate adipisci sunt, ut consectetur beatae eius inventore 
-                    quidem dolor error aperiam delectus. Odio.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Odio neque veniam, earum, sed animi nesciunt architecto
-                    voluptate adipisci sunt, ut consectetur beatae eius inventore 
-                    quidem dolor error aperiam delectus. Odio.
-                </p>
-            </div>
+  return (
+    <div>
+      <div className="Backgroung-img" >
+        <img src={homeBg} alt="" height="667px" width="100%" position="fixed" />
+        <div className="content">
+          <p>
+            <div className="w1">LIVE YOUR</div>
+            <div className="w2">ADVENTURE</div>
+            <div className="p">Don't wait until tomorrow, discover your<br />
+              adventure now and feel the sensation of<br /> closeness
+              to nature around you</div>
+          </p>
         </div>
-    )
+        <div className="box" />
+        <div><input className="destination1" placeholder="Where are you starting?" /></div>
+        <div><input className="destination2" placeholder="Where are you going?" /></div>
+        <Button href="/googleMapApi" sx={{ marginLeft: "auto", color: "white", backgroundColor: "#E86E18", top: "-28.5vh", left: "87.5vh", borderRadius: "27px" }}>Go</Button>
+      </div>
+      <div>
+        <p className="p2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Odio neque veniam, earum, sed animi nesciunt architecto
+          voluptate adipisci sunt, ut consectetur beatae eius inventore
+          quidem dolor error aperiam delectus. Odio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Odio neque veniam, earum, sed animi nesciunt architecto
+          voluptate adipisci sunt, ut consectetur beatae eius inventore
+          quidem dolor error aperiam delectus. Odio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Odio neque veniam, earum, sed animi nesciunt architecto
+          voluptate adipisci sunt, ut consectetur beatae eius inventore
+          quidem dolor error aperiam delectus. Odio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Odio neque veniam, earum, sed animi nesciunt architecto
+          voluptate adipisci sunt, ut consectetur beatae eius inventore
+          quidem dolor error aperiam delectus. Odio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Odio neque veniam, earum, sed animi nesciunt architecto
+          voluptate adipisci sunt, ut consectetur beatae eius inventore
+          quidem dolor error aperiam delectus. Odio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Odio neque veniam, earum, sed animi nesciunt architecto
+          voluptate adipisci sunt, ut consectetur beatae eius inventore
+          quidem dolor error aperiam delectus. Odio.
+
+
+        </p>
+
+
+      </div>
+    </div>
+  )
 }
 
 export default Home;
--- client/src/pages/review/Review.jsx
+++ client/src/pages/review/Review.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Header2 from "../../componets/Header2";
 
-const Review=()=>{
-  return(
+const Review = () => {
+  return (
     <div>
-        <Header2/>
+      <Header2 />
+
+
+      xsszxCadsdfdsfds Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis dolorem totam error, eligendi adipisci provident a mollitia dolor qui quas. Pariatur, a eveniet eius iure sit atque libero amet laboriosam quo odio quis odit minima ipsam autem velit aperiam porro eos eaque similique inventore reiciendis deleniti architecto. Voluptatibus dolorem ab aperiam incidunt quos atque explicabo voluptates quaerat! Repellendus sapiente, mollitia, perspiciatis odit placeat totam ducimus provident fugiat, debitis commodi voluptate?
+
+
+
+
     </div>
   )
 }
