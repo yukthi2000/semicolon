86438ed3244cc8747360ac5595bd7e668aac210b
Harshana Batagalla <harshanabatagalla@gmail.com>
2023-03-25 23:21:30 +0530

    Debug tripday rain

--- client/src/pages/gallery/Gallery.jsx
+++ client/src/pages/gallery/Gallery.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 
 const Gallery=()=>{
 
-  const tripDate = new Date('2023-03-30');
+  const tripDate = new Date('2023-04-11');
 
   //pass changed location from child components to this cmponent
   const [globalLocation, setGlobalLocation] = useState('Kandy');
--- client/src/pages/weatherApi/TripDayForecast.js
+++ client/src/pages/weatherApi/TripDayForecast.js
@@ -14,7 +14,7 @@ const TripDayForecast = (props) => {
     const [windSpeed, setWindSpeed] = useState(null);
     const [humidity, setHumidity] = useState(null);
     const [iconID, setIconId] = useState(null);
-    const [rain, setRain] = useState(null);
+    const [rain, setRain] = useState();
     const [sunrise, SetSunrise] = useState(null);
     const [sunset, SetSunset] = useState(null);
 
@@ -30,7 +30,7 @@ const TripDayForecast = (props) => {
                 .catch((error) => {
                     console.log(error);
                 });
-      
+
         }
     }
 
@@ -66,10 +66,17 @@ const TripDayForecast = (props) => {
             setFeelsLike(data.list[dateIndex] && data.list[dateIndex].feels_like && data.list[dateIndex].feels_like.day.toFixed());
             setWindSpeed(data.list[dateIndex] && data.list[dateIndex].speed);
             setHumidity(data.list[dateIndex] && data.list[dateIndex].humidity);
-            setRain(data.list[dateIndex] && data.list[dateIndex].rain)
             setIconId(data.list[dateIndex] && data.list[dateIndex].weather[0] && data.list[dateIndex].weather[0].icon);
-            SetSunrise(unixToTime(data.list[dateIndex] && data.list[dateIndex].sunrise))
-            SetSunset(unixToTime(data.list[dateIndex] && data.list[dateIndex].sunset))
+            SetSunrise(unixToTime(data.list[dateIndex] && data.list[dateIndex].sunrise));
+            SetSunset(unixToTime(data.list[dateIndex] && data.list[dateIndex].sunset));
+
+            if (data.list[dateIndex] && data.list[dateIndex].rain) {
+                setRain(data.list[dateIndex] && data.list[dateIndex].rain);
+            }
+            else {
+                setRain('N/A')
+            }
+
 
         }
     }, [data]);
@@ -77,45 +84,45 @@ const TripDayForecast = (props) => {
 
 
     return (
-              <WeatherDisplay
-              temperature={temperature}
-                description={description}
-                feelsLike={feelsLike}
-                windSpeed={windSpeed}
-                humidity={humidity}
-                iconID={iconID}
-                weekday={weekday}
-                month={month}
-                day={day}
-                rain={rain}
-                sunrise={sunrise}
-                sunset={sunset}
-            />
+        <WeatherDisplay
+            temperature={temperature}
+            description={description}
+            feelsLike={feelsLike}
+            windSpeed={windSpeed}
+            humidity={humidity}
+            iconID={iconID}
+            weekday={weekday}
+            month={month}
+            day={day}
+            rain={rain}
+            sunrise={sunrise}
+            sunset={sunset}
+        />
 
-            //  <input
-            //     value={location}
-            //     onChange={event => setLocation(event.target.value)}
-            //     onKeyPress={searchLocation}
-            //     placeholder='Enter Location'
-            //     type="text" />
-            // <br />
-            // {TripDateString}
+        //  <input
+        //     value={location}
+        //     onChange={event => setLocation(event.target.value)}
+        //     onKeyPress={searchLocation}
+        //     placeholder='Enter Location'
+        //     type="text" />
+        // <br />
+        // {TripDateString}
 
-            // {data.code !== undefined &&
-            //     <div className="content">
+        // {data.code !== undefined &&
+        //     <div className="content">
 
-            //         City = {data.city && data.city.name} <br />
-            //         Temp = {data.list[dateIndex].temp && data.list[dateIndex].temp.day.toFixed()} °C<br />
-            //         description = {data.list[dateIndex] && data.list[dateIndex].weather[0] && data.list[dateIndex].weather[0].description} <br />
-            //         feels like = {data.list[dateIndex] && data.list[dateIndex].feels_like && data.list[dateIndex].feels_like.day.toFixed()} °C<br />
-            //         Wind = {data.list[dateIndex] && data.list[dateIndex].speed} m/s <br />
-            //         Humidity = {data.list[dateIndex] && data.list[dateIndex].humidity}%<br />
-            //         date = {data.list[dateIndex].dt}
-            //         <br />
-            // } 
+        //         City = {data.city && data.city.name} <br />
+        //         Temp = {data.list[dateIndex].temp && data.list[dateIndex].temp.day.toFixed()} °C<br />
+        //         description = {data.list[dateIndex] && data.list[dateIndex].weather[0] && data.list[dateIndex].weather[0].description} <br />
+        //         feels like = {data.list[dateIndex] && data.list[dateIndex].feels_like && data.list[dateIndex].feels_like.day.toFixed()} °C<br />
+        //         Wind = {data.list[dateIndex] && data.list[dateIndex].speed} m/s <br />
+        //         Humidity = {data.list[dateIndex] && data.list[dateIndex].humidity}%<br />
+        //         date = {data.list[dateIndex].dt}
+        //         <br />
+        // } 
 
 
-            
+
     );
 }
 
