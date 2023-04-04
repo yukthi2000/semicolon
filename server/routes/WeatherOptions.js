37655a027a576348866f409dddc50ef4d1f7af72
Harshana Batagalla <harshanabatagalla@gmail.com>
2023-04-04 14:38:47 +0530

    backend

--- server/routes/WeatherOptions.js
+++ server/routes/WeatherOptions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {WeatherOptions} = require("../models");
-const bcrypt = require("bcrypt")
+
 
 router.get('/', async (req, res) =>{
     const weatherResponses = await WeatherOptions.findAll();
@@ -15,31 +15,6 @@ router.post('/', async (req,res)=>{
     res.json(weatherOptions);
 });
 
-// router.post("/", async (req, res)=>{
-//     const {name, email, password, confirmPassword} = req.body;
-//     bcrypt.hash(password, 10).then((hash)=>{
-//         User.create({
-//             name: name,
-//             email: email,
-//             password: hash,
-//             confirmPassword: confirmPassword,
-//         });
-//         res.json("success")
-//     })
-// })
 
-// router.post("/login", async (req, res)=>{
-//     const{email, password} = req.body;
-
-//     const user = await User.findOne({where: {email : email}})
-
-//     if(!user) res.json({error:"User doesnt exist"});
-
-//     bcrypt.compare(password, user.password).then((match)=>{
-//         if(!match) res.json({error:"Wrong username and password combination"})
-//     })
-
-//     res.json("YOU LOGGED IN!!!")
-// })
 
 module.exports=router;
