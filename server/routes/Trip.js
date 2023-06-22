07a02d14fd7c09c5f4573486193942ae910ca9a8
yukthi hettiarachchi <101061736+yukthi2000@users.noreply.github.com>
2023-06-22 14:56:19 +0530

    trip view working

--- server/routes/Trip.js
+++ server/routes/Trip.js
@@ -193,3 +193,136 @@ module.exports = router;
 //   await Ratings.create(post);
 //   res.json(post);
 // });
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// const { Trip } = require("../models");
+// const { Locations } = require("../models");
+// const { validateToken } = require("../middlewares/AuthMiddleware");
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const trips = await Trip.findAll();
+//     res.status(200).json(trips);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to retrieve trips" });
+//   }
+// });
+
+// router.get("/data", validateToken, async (req, res) => {
+//   try {
+//     const userId = req.user.id;
+//     const trips = await Trip.findAll({ where: { userId } });
+//     //res.json(trips);
+//     res.status(200).json(trips);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to retrieve trips" });
+//   }
+// });
+
+// // router.get("/dataWithLocations", async (req, res) => {
+// //   try {
+// //     const trips = await Trip.findAll({
+// //       include: {
+// //         model: Locations,
+// //         attributes: ["name"],
+// //       },
+// //     });
+// //     res.json(trips);
+// //   } catch (error) {
+// //     console.error(error);
+// //     res.status(500).json({ error: "Failed to fetch trip details" });
+// //   }
+// // });
+
+// router.get("/dataWithLocations", async (req, res) => {
+//   try {
+//     const trips = await Trip.findAll({
+//       include: [
+//         {
+//           model: Locations,
+//           attributes: ["name"],
+//         },
+//       ],
+//     });
+//     res.json(trips);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to fetch trip details" });
+//   }
+// });
+
+// router.get("/dataWithLocationsForUser", validateToken, async (req, res) => {
+//   try {
+//     const userId = req.user.id;
+
+//     const trips = await Trip.findAll({
+//       where: { userId },
+//       include: [
+//         {
+//           model: Locations,
+//           attributes: ["name"],
+//         },
+//       ],
+//     });
+
+//     res.json(trips);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to fetch trip details" });
+//   }
+// });
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const tripData = req.body;
+//     const createdTrip = await Trip.create(tripData);
+//     res.status(201).json(createdTrip);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to create a trip" });
+//   }
+// });
+
+// router.post("/tripdata", validateToken, async (req, res) => {
+//   try {
+//     const tripData = req.body;
+//     const userId = req.user.id;
+//     tripData.userId = userId;
+//     const createdTrip = await Trip.create(tripData);
+//     const createdTripId = createdTrip.id;
+//     console.log(createdTripId);
+//     res.status(201).json({ tripId: createdTripId });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to create a trip" });
+//   }
+// });
+
+// router.delete("/trips/:id", async (req, res) => {
+//   const tripId = req.params.id;
+
+//   try {
+//     // Find the trip by ID
+//     const trip = await Trip.findByPk(tripId);
+
+//     if (!trip) {
+//       return res.status(404).json({ error: "Trip not found" });
+//     }
+
+//     // Delete the trip
+//     await trip.destroy();
+
+//     res.status(200).json({ message: "Trip deleted successfully" });
+//   } catch (error) {
+//     console.error("Error deleting trip:", error);
+//     res.status(500).json({ error: "Failed to delete trip" });
+//   }
+// });
+
+
+// module.exports = router;
